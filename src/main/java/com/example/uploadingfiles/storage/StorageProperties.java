b3eea8443b55974dc9590d48bd44002119ae9916
Zhang Guanping <74082784+arraycat@users.noreply.github.com>
2021-02-20 16:48:41 +0800

    Update README.MD

--- src/main/java/com/example/uploadingfiles/storage/StorageProperties.java
+++ src/main/java/com/example/uploadingfiles/storage/StorageProperties.java
@@ -9,8 +9,6 @@ public class StorageProperties {
 	 * Folder location for storing files
 	 */
 
-	//linux中，war方式运行时，非tmp目录可能报错
-	//Window中，jar方式运行时，可能会把所在盘符作为根目录，F:\tmp\spring\upload
 	private String location = "/tmp/spring/upload";
 
 	public String getLocation() {
